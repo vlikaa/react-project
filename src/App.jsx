7011bf611efe404e72e2716012772f5b4a5c7369
Ali Veliyev <aliveliyev123654@gmail.com>
2024-12-19 11:15:54 +0400

    added Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Characters from "./components/Characters/Characters";
 import Pagination from "./components/Pagination/Pagination";
+import Loader from "./components/Loader/Loader";
 
 function App() {
-	const [charactersData, setCharactersData] = useState({ info: {pages: 0, prev: null, next: null}, results: [] });
+	const [charactersData, setCharactersData] = useState({
+		info: { pages: 0, prev: null, next: null },
+		results: [],
+	});
 	const [url, setUrl] = useState("https://rickandmortyapi.com/api/character");
 	const [currentPage, setCurrentPage] = useState(1);
+	const [isLoading, setIsLoading] = useState(false);
+	const [hasError, setHasError] = useState(false);
 
 	useEffect(() => {
+		setIsLoading(true);
+		setHasError(false); // сбрасываем ошибку при попытке нового запроса
+
 		fetch(url)
-			.then(r => r.json())
+			.then(r => {
+				if (!r.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return r.json();
+			})
 			.then(data => {
 				setCharactersData( () => ({
 					info: data.info,
 					results: data.results
 				}));
-			});
+			})
+			.catch(e => {
+				setHasError(true);
+				console.error(e);
+			})
+			.finally(() => setIsLoading(false));
 	}, [url]);
 
 	return (
 		<>
 			<main>
-				<Characters charactersData={charactersData.results} />
-				<Pagination info={charactersData.info} setUrl={setUrl} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+				{isLoading || hasError ? (
+					<Loader />
+				) : (
+					<>
+						<Characters charactersData={charactersData.results} />
+						<Pagination
+							info={charactersData.info}
+							setUrl={setUrl}
+							currentPage={currentPage}
+							setCurrentPage={setCurrentPage}
+						/>
+					</>
+				)}
 			</main>
 		</>
 	);
